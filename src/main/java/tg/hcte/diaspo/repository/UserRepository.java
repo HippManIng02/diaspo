2fecec84aeaaea786c55f323b1ac5b4e69443e5b
denisbyte <denisafoh22@gmail.com>
2026-04-15 18:14:23 +0200

    Création du fichier JWtService et Création du package dto

---
+++ src/main/java/tg/hcte/diaspo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package tg.hcte.diaspo.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tg.hcte.diaspo.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
