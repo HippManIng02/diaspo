e944998fa2af34c68b3aad84ab80db5ee4858af2
HippManIng02 <yao-hippolyte.folly@ens.uvsq.fr>
2026-04-15 14:30:48 +0200

    initial commit

---
+++ src/main/java/tg/hcte/diaspo/DiaspoApplication.java
@@ -0,0 +1,13 @@
+package tg.hcte.diaspo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaspoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaspoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tg/hcte/diaspo/DiaspoApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.hcte.diaspo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaspoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
